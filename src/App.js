c32d95601c94e3d5e747e5cb9346fe7c5773dd3b
Ilia Makhonin <noizemkutane77@gmail.com>
2019-08-21 01:44:22 +0300

    Code of the App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React.js!</h1>
+            </div>
+        );
+    }
+}
+
+export default hot(App);
